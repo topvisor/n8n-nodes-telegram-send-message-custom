c65df05afa6f347e75dcb7d0d4cfea24264a4f39
Artemeey <artemeey@mail.ru>
2025-08-29 15:33:55 +0300

    feat(nodes,config): add Telegram Send Custom Message node with configurable JSON, setup TypeScript config and README

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# n8n-nodes-telegram-send-message-custom
-Node for n8n to send to Telegram in the any format
+# 🤖 n8n-nodes-telegram-send-message-custom
+
+✨ **Differences from the standard Telegram Send Message node in n8n:**
+
+- 📝 You can provide **any data** supported by the Telegram API without limitations
+    - either manually as a JSON string or as an object
+    - or using variables obtained from previous steps
+- 🔍 The **Output** panel displays raw API request/response data, making debugging more transparent and straightforward
+
+🚀 Custom n8n node to send Telegram messages using `telegramApi` credentials with **custom JSON**.
+
+For example, adding custom `reply_markup` in JSON (inline keyboard, reply keyboard etc):
+
+```json
+{
+  "reply_markup": {
+    "inline_keyboard": [
+      [
+        {
+          "text": "🌐 Open Website",
+          "url": "https://example.com"
+        }
+      ],
+      [
+        {
+          "text": "✅ Confirm",
+          "callback_data": "confirm_action"
+        }
+      ]
+    ]
+  }
+}
+```
+
+📚 See the official [Telegram Bot API docs](https://core.telegram.org/bots/api#sendmessage).
+
+---
+
+## ⚙️ Install
+
+In n8n:
+**Settings → Community Nodes → Install → `n8n-nodes-telegram-send-message-custom`**
+
+---
+
+## 🛠️ Usage
+
+1. ➕ Add this node in workflow
+2. 🔑 Select your Telegram API credentials
+3. 💬 Enter Chat ID, Text
+4. 🧩 Paste raw JSON into **Custom JSON** (supports expressions)
---
+++ src/TelegramSendMessageCustom.node.ts
@@ -0,0 +1,72 @@
+import type {IDataObject, IExecuteFunctions, INodeType, INodeTypeDescription} from 'n8n-workflow'
+import {NodeConnectionType,} from 'n8n-workflow';
+
+export class TelegramSendMessageCustom implements INodeType {
+    description: INodeTypeDescription = {
+        displayName: 'Telegram Send Custom Message',
+        name: 'telegramSendMessageCustom',
+        icon: 'file:telegram.svg',
+        group: ['transform'],
+        version: 1,
+        description: 'Send Telegram message using credentials and custom JSON',
+        documentationUrl: 'https://core.telegram.org/bots/api#sendmessage',
+        defaults: {name: 'Telegram Send Custom Message'},
+        inputs: [NodeConnectionType.Main],
+        outputs: [NodeConnectionType.Main],
+        credentials: [{name: 'telegramApi', required: true}],
+        properties: [
+            {displayName: 'Chat ID', name: 'chatId', type: 'string', default: ''},
+            {displayName: 'Text', name: 'text', type: 'string', default: ''},
+            {
+                displayName: 'Custom JSON',
+                name: 'customJson',
+                type: 'json',
+                default: {},
+                description: 'Custom JSON object will be added to the request body',
+                hint: `Example: { "reply_markup": { "inline_keyboard": [[{ "text": "Open site", "url": "https://example.com" }], [{ "text": "Run callback", "callback_data": "do_action" }]] }}`,
+            }
+        ],
+    }
+
+    async execute(this: IExecuteFunctions) {
+        const items = this.getInputData()
+        const creds = await this.getCredentials('telegramApi') as { accessToken: string }
+        const token = creds.accessToken
+        const results: Array<{ json: IDataObject }> = []
+
+        for (let i = 0; i < items.length; i++) {
+            const chatId = this.getNodeParameter('chatId', i) as string
+            const text = this.getNodeParameter('text', i) as string
+            let customJson = this.getNodeParameter('customJson', i) as IDataObject | string
+
+            let customJsonObjectr: Record<string, any>;
+
+            if (typeof customJson === 'string') {
+                try {
+                    customJsonObjectr = JSON.parse(customJson)
+                } catch {
+                    throw new Error('Custom JSON must be valid JSON')
+                }
+            } else {
+                customJsonObjectr = customJson;
+            }
+
+            let body: IDataObject = {chat_id: chatId, text}
+
+            if (Object.keys(customJson).length > 0) {
+                body = {...body, ...customJsonObjectr}
+            }
+
+            const res = await this.helpers.httpRequest({
+                method: 'POST',
+                url: `https://api.telegram.org/bot${token}/sendMessage`,
+                body,
+                json: true
+            })
+
+            results.push({json: {request: body, response: res as IDataObject}})
+        }
+
+        return this.prepareOutputData(results)
+    }
+}
